067a531685c5993133d5e42d91cddb6385dd45fe
kaiqiangren <1006042475@qq.com>
2019-10-23 16:39:03 +0800

    fe-button css

--- examples/App.vue
+++ examples/App.vue
@@ -1,15 +1,12 @@
 <template>
     <div>
         <fe-button>
-            fe-button123123132
-        </fe-button>
-        <fe-button>
-            fe-button123123132
+            按钮测试
         </fe-button>
     </div>
 </template>
-<style>
-    @import "../lib/theme/main.css";
+<style lang="scss">
+    @import "../packages/theme/src/button.scss";
 </style>
 <script>
     import FeButton from "../packages/button/src/button";
